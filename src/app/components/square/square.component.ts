73ad053f702bbb862967d233ff25e9c9bf38df2b
Kent Zamudio <kazamudio@up.edu.ph>
2021-06-30 23:32:57 +0800

    PWA and all unctionalities added

---
+++ src/app/components/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';                         
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button nbButton hero shape="semi-round" status="primary" *ngIf="!value">{{ value }}</button>
+    <button nbButton hero shape="semi-round" status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero shape="semi-round" status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
+})
+export class SquareComponent  {
+
+  @Input() value!: "X" | "O";
+
+}
+
